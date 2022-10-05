d4bd48d84a1aab0eecb2bae108550f4fe9589541
yahav <yahav2324@gmail.com>
2022-10-05 22:08:46 +0300

    setup sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from "express";
+import db from './config/data/database.config';
 
+db.sync().then(() => {console.log("connecting to db")});
 const port = 3000;
 const app = express();
 
