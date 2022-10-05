3a5f25aab2333995432c20dc037ae17ac207e5c5
yahav <yahav2324@gmail.com>
2022-10-06 00:25:48 +0300

    added get request and convantion

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, {NextFunction, Request, Response} from "express";
+import express, {Request, Response} from "express";
+import expenseValidator from "./validator/index";
 import db from './config/data/database.config';
 import {v4  as uuidv4} from 'uuid';
 import { expensesTable } from "./model";
-import expenseValidator from "./validator/index";
-import {validationResult} from 'express-validator';
+import Middleware from './middleware/handleValidation'
 
 db.sync().then(() => {console.log("connecting to db")});
 const port = 3000;
@@ -12,14 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.post("/expense",
+    Middleware.handleValidationError,
     expenseValidator.checkCreateExpense(),
-    (req: Request, res: Response, next: NextFunction) => {
-        const error = validationResult(req);
-        if(!error.isEmpty()){
-            return res.json(error);
-        }
-        next();
-    },
     async (req: Request, res: Response) => {
     const id = uuidv4();
     try {
@@ -27,6 +21,17 @@ app.post("/expense",
     return res.json({record, msg: 'successfully create todo'});
     }catch(e){
         return res.json({msg: "fail to create", status: 500, route: '/expense'})
+    }
+});
+
+app.get('/expense', async (req: Request, res: Response) => {
+    const limit= req.query?.limit as number | undefined;
+    console.log(limit);
+    try{
+        const records = await expensesTable.findAll({where:{}, limit});
+        return res.json(records);
+    }catch(e){
+        return res.json({msg: "fail to read", status: 500, route: '/expense'})
     }
 });
 
