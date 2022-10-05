820246a5eb5ea6c94a6597efc6c8b57a6441b51d
yahav <yahav2324@gmail.com>
2022-10-05 21:43:59 +0300

    setup express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log("hello world");
+import express, {Request, Response} from "express";
+
+const port = 3000;
+const app = express();
+
+app.listen(port, () => console.log('server is listening on port ' + port));
